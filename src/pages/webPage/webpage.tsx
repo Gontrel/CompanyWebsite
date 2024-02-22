efe053345a52580581b908123df2deca9f0f1233
vincent ogbe <vincentogbe085@gmail.com>
2024-02-22 22:55:37 +0000

    webpage update

--- src/pages/webPage/webpage.tsx
+++ src/pages/webPage/webpage.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faFire } from "@fortawesome/free-solid-svg-icons"; // Import the necessary icon
 import "./webpage.css";
+import { handleFirestore } from "../../lib/firebase";
+import { doc } from "firebase/firestore";
 
 const Webpage: React.FC = () => {
+  function handleSubmit() {
+    const emailInput = document.querySelector(
+      ".emailInput"
+    ) as HTMLInputElement | null;
+
+    if (emailInput) {
+      const email = emailInput.value;
+      doc(handleFirestore(), "email", email);
+    } else {
+      alert("Email field is emapty.");
+    }
+  }
+
   return (
     <div className="webPage">
       <div>
@@ -27,7 +42,17 @@ const Webpage: React.FC = () => {
             go.
           </p>
           <h6>Interested?! Keep up-to date with Gontrel below</h6>
-          <button className="submitButton">Enter email here </button>
+
+          <div className="inputWithButton">
+            <input
+              type="text"
+              className="emailInput"
+              placeholder="Enter email here"
+            />
+            <button className="submitButton" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
         </div>
       </div>
       <img
