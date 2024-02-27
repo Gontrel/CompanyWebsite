b4e4680ed95d7dca9c3ae7f81e79b229594442cb
Godstime Osarobo <nosarobo@gmail.com>
2024-02-27 02:38:47 +0000

    Added fonts, Tailwind and fixed email to firebase issue

--- src/pages/webPage/webpage.tsx
+++ src/pages/webPage/webpage.tsx
@@ -1,70 +1,137 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faFire } from "@fortawesome/free-solid-svg-icons"; // Import the necessary icon
-import "./webpage.css";
+import "./webpage.scss";
 import { handleFirestore } from "../../lib/firebase";
 import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 const Webpage: React.FC = () => {
-  function handleSubmit() {
-    const emailInput = document.querySelector(
-      ".emailInput"
-    ) as HTMLInputElement;
-
-    if (emailInput) {
-      const email = emailInput.value;
-      const emailsCollection = collection(handleFirestore(), "emails");
-      addDoc(emailsCollection, { email })
-        .then(() => console.log("Email added successfully"))
-        .catch((error) => console.error("Error adding email: ", error));
-    } else {
-      alert("Email field is empty.");
+    const [email, setEmail] = useState("");
+    const [emailErrorText, setEmailErrorText] = useState("");
+    const [emailSuccess, setEmailSuccess] = useState(false);
+    function handleSubmit() {
+        if (email) {
+            const emailsCollection = collection(handleFirestore(), "emails");
+            addDoc(emailsCollection, { email })
+                .then(() => {
+                    console.log("Email added successfully");
+                    setEmailSuccess(true);
+                })
+                .catch((error) => console.error("Error adding email: ", error))
+                .finally(() => {
+                    setTimeout(() => {
+                        setEmail("");
+                    }, 3000);
+                });
+        }
     }
-  }
 
-  return (
-    <div className="webPage">
-      <div>
-        <img src="./src/assets/GontrelLogo.png" alt="Logo" className="logo" />
-        <button className="roundedButton">
-          App coming soon <FontAwesomeIcon icon={faEye} />
-          <FontAwesomeIcon icon={faFire} style={{ color: "#ff0000" }} />
-        </button>
-        <div className="webPageText">
-          <h1>
-            Discover,
-            <br />
-            Experience and Share with <span className="blueText">Gontrel</span>
-          </h1>
-          <br />
-          <p>
-            The ultimate way to explore exciting events, thrilling
-            <br />
-            activities, and delicious meals in your area or wherever you
-            <br />
-            go.
-          </p>
-          <h6>Interested?! Keep up-to date with Gontrel below</h6>
+    useEffect(() => {
+        if (email) {
+            // check if email is valid
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                setEmailErrorText(
+                    "Email is invalid! Please enter a valid email."
+                );
+                setEmailSuccess(false);
+            } else {
+                setEmailErrorText("");
+            }
+        } else {
+            setEmailErrorText("");
+            setEmailSuccess(false);
+        }
+    }, [email]);
 
-          <div className="inputWithButton">
-            <input
-              type="text"
-              className="emailInput"
-              placeholder="Enter email here"
+    return (
+        <div className="webPage">
+            <div>
+                <img
+                    src="./src/assets/GontrelLogo.png"
+                    alt="Logo"
+                    className="logo"
+                />
+                <button className="roundedButton">
+                    App coming soon <FontAwesomeIcon icon={faEye} />
+                    <FontAwesomeIcon
+                        icon={faFire}
+                        style={{ color: "#ff0000" }}
+                    />
+                </button>
+                <div className="webPageText">
+                    <h1>
+                        Discover,
+                        <br />
+                        experience and share with
+                        <span className="blueText"> Gontrel</span>
+                    </h1>
+                    <br />
+                    <p>
+                        The ultimate way to explore exciting events, thrilling
+                        <br />
+                        activities, and delicious meals in your area or wherever
+                        you
+                        <br />
+                        go.
+                    </p>
+                    <h6>Interested?! Keep up-to date with Gontrel below</h6>
+
+                    <div
+                        className={`inputWithButton
+                        ${emailErrorText.length > 0 ? "border-red-500" : ""}
+                        ${
+                            email.length > 0 && emailErrorText.length == 0
+                                ? "border-green-500"
+                                : ""
+                        }
+                        `}>
+                        <input
+                            type="text"
+                            className="emailInput"
+                            placeholder="Enter email here"
+                            value={email}
+                            onChange={(e) =>
+                                setEmail((e.target as HTMLInputElement).value)
+                            }
+                            onInput={(e) =>
+                                setEmail((e.target as HTMLInputElement).value)
+                            }
+                        />
+                        <button
+                            onClick={handleSubmit}
+                            className="submitButton"
+                            disabled={
+                                emailErrorText.length > 0 || email.length === 0
+                            }>
+                            Submit
+                        </button>
+                    </div>
+                    {emailErrorText.length > 0 && (
+                        <p
+                            className={`errorText
+                            ${emailErrorText.length > 0 ? "visible" : "hidden"}
+                            `}>
+                            {emailErrorText}
+                        </p>
+                    )}
+                    {{ emailSuccess } && (
+                        <p
+                            className={`successText
+                            ${emailSuccess ? "visible" : "hidden"}
+                            `}>
+                            Email submitted successfully!
+                        </p>
+                    )}
+                </div>
+            </div>
+            <img
+                src="./src/assets/landing-background.png"
+                alt="Description"
+                className="rightImage"
             />
-            <button className="submitButton" onClick={handleSubmit}>
-              Submit
-            </button>
-          </div>
         </div>
-      </div>
-      <img
-        src="./src/assets/landing-background.png"
-        alt="Description"
-        className="rightImage"
-      />
-    </div>
-  );
+    );
 };
 
 export default Webpage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,jsx,ts,tsx,css,scss}'],
+  darkMode: 'media',
   theme: {
+    extend: {
+      animation: {
+        'spin-reverse': 'spin-reverse 1s linear infinite',
+      },
+      keyframes: {
+        'spin-reverse': {
+          '0%': { transform: 'rotate(360deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
