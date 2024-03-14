dde7f44d87d6339994e193997751a3b1565f1cd3
Godstime Osarobo <nosarobo@gmail.com>
2024-03-14 02:59:39 +0000

    Update dependencies and media queries in index.scss

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -11,6 +11,7 @@ import Snapchat from '/assets/icons/Snapchat.png';
 import Facebook from '/assets/icons/Facebook.png';
 import Twitter from '/assets/icons/Twitter.png';
 import './LandingPage.scss';
+import HeroImage from '/assets/images/LandingBackground.png';
 
 const LandingPage: React.FC = () => {
     const [email, setEmail] = useState('');
@@ -92,61 +93,70 @@ const LandingPage: React.FC = () => {
     }, [email, isLoading]);
 
     return (
-        <div id="landingPage" className="w-screen min-h-screen sm:h-screen">
-            <div className="relative flex flex-col justify-between w-auto px-4 pt-10 pb-16 sm:h-screen sm:ps-28 sm:pt-12">
-                <div className="flex-shrink mb-10 logo">
-                    <img
-                        className="h-auto w-100"
-                        src={GontrelLogo}
-                        alt="Company Logo"
-                    />
-                </div>
-                <div id="hero--content" className="flex items-start ">
-                    <div className="flex flex-col gap-y-4 content--section">
-                        <div className="flex sm:mb-6 gap-x-3 coming-soon--section">
-                            <div className="flex items-center justify-center gap-3 px-6 py-2 border-solid pointer-events-none select-none coming-soon--pill">
-                                <span className="coming-soon--text">
-                                    App coming soon!
-                                </span>
-                                <span className="coming-soon--emoji">👀</span>
-                                <span className="coming-soon--emoji">🔥</span>
+        <div
+            id="landingPage"
+            className="flex w-screen min-h-screen md:h-screen"
+        >
+            <div className="flex flex-1 landing-page--left 2xl:items-center 2xl:justify-end">
+                <div className="flex flex-col items-center justify-between w-screen px-4 pt-10 pb-16 2xl:w-auto md:items-start md:h-screen md:ps-28 md:pt-12">
+                    <div className="flex-shrink mb-10 logo md:pe-80 md:me-4">
+                        <img
+                            className="w-auto h-auto"
+                            src={GontrelLogo}
+                            alt="Company Logo"
+                        />
+                    </div>
+                    <div id="hero--content" className="flex items-start ">
+                        <div className="flex flex-col gap-y-4 content--section">
+                            <div className="flex md:mb-6 gap-x-3 coming-soon--section">
+                                <div className="flex items-center justify-center gap-3 px-6 py-2 border-solid pointer-events-none select-none coming-soon--pill">
+                                    <span className="coming-soon--text">
+                                        App coming soon!
+                                    </span>
+                                    <span className="coming-soon--emoji">
+                                        👀
+                                    </span>
+                                    <span className="coming-soon--emoji">
+                                        🔥
+                                    </span>
+                                </div>
+                                <div>
+                                    <img
+                                        className="coming-soon--icon"
+                                        alt="heart icon"
+                                        src={Heart}
+                                    />
+                                </div>
                             </div>
-                            <div>
-                                <img
-                                    className="coming-soon--icon"
-                                    alt="heart icon"
-                                    src={Heart}
-                                />
+                            <div className="flex flex-col max-w-sm md:max-w-md hero--text gap-y-4 md:gap-y-5">
+                                <h1>
+                                    Discover,
+                                    <br />
+                                    experience and share with
+                                    <span className="text-gontrel-blue">
+                                        {' '}
+                                        Gontrel
+                                    </span>
+                                </h1>
+                                <div className="my-1 squiggle-container">
+                                    <img src={Squiggle} alt="squiggle"></img>
+                                </div>
+                                <p>
+                                    The ultimate way to explore exciting events,
+                                    thrilling activities, and delicious meals in
+                                    your area or wherever you go.
+                                </p>
                             </div>
-                        </div>
-                        <div className="flex flex-col max-w-sm sm:max-w-md hero--text gap-y-4 sm:gap-y-5">
-                            <h1>
-                                Discover,
-                                <br />
-                                experience and share with
-                                <span className="text-gontrel-blue">
-                                    {' '}
-                                    Gontrel
-                                </span>
-                            </h1>
-                            <div className="my-1 squiggle-container">
-                                <img src={Squiggle} alt="squiggle"></img>
-                            </div>
-                            <p>
-                                The ultimate way to explore exciting events,
-                                thrilling activities, and delicious meals in
-                                your area or wherever you go.
-                            </p>
-                        </div>
-                        <form
-                            className="flex flex-col max-w-sm mt-4 sm:mt-8 sm:max-w-md hero--form"
-                            onSubmit={handleSubmit}
-                        >
-                            <h6>
-                                Interested?! Keep up-to date with Gontrel below
-                            </h6>
-                            <div
-                                className={`input--container p-2 flex justify-between gap-x-8 mt-4 mb-1
+                            <form
+                                className="flex flex-col max-w-sm mt-4 md:mt-8 md:max-w-md hero--form"
+                                onSubmit={handleSubmit}
+                            >
+                                <h6>
+                                    Interested?! Keep up-to date with Gontrel
+                                    below
+                                </h6>
+                                <div
+                                    className={`input--container p-2 flex justify-between gap-x-8 mt-4 mb-1
                                     ${
                                         validHelperText.length > 0 ||
                                         submitHelperText.length > 0
@@ -159,97 +169,108 @@ const LandingPage: React.FC = () => {
                                             : ''
                                     }
                                     `}
-                            >
-                                <div className="flex flex-col flex-grow pt-2 ps-8 input-container--text">
-                                    <input
-                                        type="email"
-                                        className="email--input"
-                                        placeholder="Enter your email"
-                                        value={email}
-                                        onChange={e => setEmail(e.target.value)}
-                                        onInput={e =>
-                                            setEmail(e.currentTarget.value)
-                                        }
-                                        ref={emailInput}
-                                    />
-                                    <div className="input-helper--text min-h-4">
-                                        <HelperText
-                                            errorText={errorHelperText}
-                                            validText={validHelperText}
-                                            submitText={submitHelperText}
-                                        />
-                                    </div>
-                                </div>
-                                <button
-                                    className="flex items-center justify-center h-12 px-6 py-4 text-white rounded-md min-w-24 submit--button bg-gontrel-blue"
-                                    type="submit"
-                                    disabled={
-                                        errorHelperText.length > 0 ||
-                                        email.length === 0
-                                    }
                                 >
-                                    {isLoading ? (
-                                        <img
-                                            src={Loader}
-                                            alt="loading"
-                                            className="loader animate-spin-reverse"
+                                    <div className="flex flex-col flex-grow pt-2 ps-8 input-container--text">
+                                        <input
+                                            type="email"
+                                            className="email--input"
+                                            placeholder="Enter your email"
+                                            value={email}
+                                            onChange={e =>
+                                                setEmail(e.target.value)
+                                            }
+                                            onInput={e =>
+                                                setEmail(e.currentTarget.value)
+                                            }
+                                            ref={emailInput}
                                         />
-                                    ) : (
-                                        'Submit'
-                                    )}
-                                </button>
-                            </div>
-                        </form>
+                                        <div className="input-helper--text min-h-4">
+                                            <HelperText
+                                                errorText={errorHelperText}
+                                                validText={validHelperText}
+                                                submitText={submitHelperText}
+                                            />
+                                        </div>
+                                    </div>
+                                    <button
+                                        className="flex items-center justify-center h-12 px-6 py-4 text-white rounded-md min-w-24 submit--button bg-gontrel-blue"
+                                        type="submit"
+                                        disabled={
+                                            errorHelperText.length > 0 ||
+                                            email.length === 0
+                                        }
+                                    >
+                                        {isLoading ? (
+                                            <img
+                                                src={Loader}
+                                                alt="loading"
+                                                className="loader animate-spin-reverse"
+                                            />
+                                        ) : (
+                                            'Submit'
+                                        )}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="flex-shrink hero--footer">
+                        <div className="flex-row hidden md:flex icon--container gap-x-9">
+                            <img
+                                className="w-10 h-10"
+                                alt="heart icon"
+                                src={Instagram}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.instagram.com/gontrel.app/',
+                                        '_blank'
+                                    )
+                                }
+                            />
+                            <img
+                                className="w-10 h-10"
+                                alt="heart icon"
+                                src={Snapchat}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.snapchat.com/gontrel.app/',
+                                        '_blank'
+                                    )
+                                }
+                            />
+                            <img
+                                className="w-10 h-10"
+                                alt="heart icon"
+                                src={Facebook}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.facebook.com/gontrel.app/',
+                                        '_blank'
+                                    )
+                                }
+                            />
+                            <img
+                                className="w-10 h-10"
+                                alt="heart icon"
+                                src={Twitter}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.twitter.com/gontrel.app/',
+                                        '_blank'
+                                    )
+                                }
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="flex-shrink hero--footer">
-                    <div className="flex-row hidden sm:flex icon--container gap-x-9">
-                        <img
-                            className="w-10 h-10"
-                            alt="heart icon"
-                            src={Instagram}
-                            onClick={() =>
-                                window.open(
-                                    'https://www.instagram.com/gontrel.app/',
-                                    '_blank'
-                                )
-                            }
-                        />
-                        <img
-                            className="w-10 h-10"
-                            alt="heart icon"
-                            src={Snapchat}
-                            onClick={() =>
-                                window.open(
-                                    'https://www.snapchat.com/gontrel.app/',
-                                    '_blank'
-                                )
-                            }
-                        />
-                        <img
-                            className="w-10 h-10"
-                            alt="heart icon"
-                            src={Facebook}
-                            onClick={() =>
-                                window.open(
-                                    'https://www.facebook.com/gontrel.app/',
-                                    '_blank'
-                                )
-                            }
-                        />
-                        <img
-                            className="w-10 h-10"
-                            alt="heart icon"
-                            src={Twitter}
-                            onClick={() =>
-                                window.open(
-                                    'https://www.twitter.com/gontrel.app/',
-                                    '_blank'
-                                )
-                            }
-                        />
-                    </div>
-                </div>
+            </div>
+            <div className="w-1/12"></div>
+            <div className="relative hidden landing-page--right 2xl:flex 2xl:flex-1 2xl:me-28">
+                <img
+                    className="absolute top-0 bottom-0 left-0 m-auto max-w-max w-max hero--image"
+                    alt="hero image"
+                    src={HeroImage}
+                />
             </div>
         </div>
     );
